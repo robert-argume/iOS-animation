9e01895d1bd6607b2b85162b9b8a54ebab9b43b6
Robert <robert.a.argume@gmail.com>
2018-04-23 19:54:42 -0400

    Add animations on StartViewController

--- Lesson13/View Controllers/StartViewController.swift
+++ Lesson13/View Controllers/StartViewController.swift
@@ -10,8 +10,10 @@ import UIKit
 
 class StartViewController: UIViewController {
 
-    @IBOutlet weak var StartScreenLabel: NSLayoutConstraint!
+    @IBOutlet weak var StartViewLabel: UILabel!
     
+    
+    @IBOutlet weak var NextButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,11 +21,34 @@ class StartViewController: UIViewController {
 
   
     override func viewDidAppear(_ animated: Bool) {
-        
+        AnimateLabelIn()
+        AnimateButtonIn()
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        
+        StartViewLabel.alpha = 0
+        NextButton.isHidden = true
+    }
+    
+    func AnimateLabelIn(){
+        UIView.animate(withDuration: 1, delay: 0.25, options:[.curveEaseInOut], animations:{
+            self.StartViewLabel.alpha = 1
+            self.StartViewLabel.center.y -= 50
+            //self.StartViewLabel.frame.size = CGAffineTransform(scaleX: 2.0, y: 2.0)
+        }, completion: nil)
+    }
+    
+    func AnimateButtonIn(){
+        UIView.transition(with: NextButton, duration: 2, options: [.transitionFlipFromTop], animations: {
+                self.NextButton.isHidden = false
+            
+            }, completion: MoveButtonOver )
+    }
+    
+    func MoveButtonOver(a: Bool){
+        UIView.animate(withDuration: 1, delay: 0.25, options: [.curveEaseIn], animations: {
+            self.NextButton.center.x += 125
+            }, completion: nil)
     }
     
     @IBAction func NextButton(_ sender: UIButton) {
